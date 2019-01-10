cd1df05ce0796f97d7da312534725592d7d28fed
Veyhunk <Veyhunk@qq.com>
2019-01-10 11:00:45 +0800

    add

---
+++ css水波动画/5iweb2016110402/js/index.js
@@ -0,0 +1,11 @@
+// var waveOneinc = 0;
+// var waveTwoinc = 25;
+// var waveThreeinc = 35;
+// setInterval(function() {
+//     $('#wave-one').css('transform', 'rotate(' + waveOneinc + 'deg)');
+//     waveOneinc += 1.15;
+//     waveTwoinc += 1.25;
+//     waveThreeinc += 1.35;
+//     $('#wave-two').css('transform', 'rotate(' + waveTwoinc + 'deg)');
+//     $('#wave-three').css('transform', 'rotate(-' + waveThreeinc + 'deg)');
+// }, 20);
